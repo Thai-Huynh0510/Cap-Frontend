16435bb822e907c143f790adc57a39986c7e055e
Kristina <112591859+KristinaD31@users.noreply.github.com>
2023-03-29 15:11:16 -0400

    added route

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import {Switch, Route} from 'react-router-dom'
 
 import Homepage from './Components/HomePage';
 import Tasks from './Components/Tasks';
+import Employees from './components/Employees';
+
 const App = () => {
   return (
     <div className="App">
       <Switch>
       <Route exact path="/" component = {Homepage} />
       <Route exact path="/tasks" component={Tasks} />
+      <Route exact path="/employees" component={Employees} />
       </Switch>
     </div>
   );
